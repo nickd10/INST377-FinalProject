96bb793c3facf63deff2f38234b9881d6f1b51eb
nickd10 <nickdossin@gmail.com>
2025-05-09 14:54:01 -0400

    update

--- nodeexample/node.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const hostName = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer(() => {});
-server.listen(port, hostName, () => {
-  console.log(`Server running at http://${hostNamw}:${port}`);
-});
