c1b55449fbf8d4c05ed419687d98547c18395c14
nickd10 <nickdossin@gmail.com>
2025-05-10 00:28:20 -0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const supabaseClient = require('@supabase/supabase-js');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-const supabaseUrl = 'https://ferjvmbmtfqyztixhyea.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlcmp2bWJtdGZxeXp0aXhoeWVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MTEwMDIsImV4cCI6MjA2MjM4NzAwMn0.q7l9DIy5kZ3WH6pn8D_DymcwRrNwN2uoRUZlWJ3x5h4';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
-
+app.get('/', (req, res) => {
+    res.sendFile('public/home.html', { root: __dirname });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
