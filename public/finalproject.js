c33f9b937e492bc9adac11b8abede86f9940b05c
nickd10 <nickdossin@gmail.com>
2025-05-11 15:54:16 -0400

    update

--- public/finalproject.js
+++ public/finalproject.js
@@ -6,27 +6,34 @@
       console.log(data);
   
       list.innerHTML = '';
-  
-    data.forEach(c => {
+    let i = 0;
+    data.slice(0,20).forEach(c => {
         const li = document.createElement('li');
         li.className = 'carouselTicker__list';
+        // li.innerHTML = `
+        //   <div class="crypto">
+        //     <h3>${c.name} (${c.symbol})</h3>
+        //     <p>Price: $${(+c.priceUsd).toFixed(2)}</p>
+        //     <p>Change (24h): ${(+c.changePercent24Hr).toFixed(2)}%</p>
+        //   </div>
+        // `;
+
         li.innerHTML = `
-          <div class="crypto">
-            <h3>${c.name} (${c.symbol})</h3>
-            <p>Price: $${(+c.priceUsd).toFixed(2)}</p>
-            <p>Change (24h): ${(+c.changePercent24Hr).toFixed(2)}%</p>
-          </div>
-        `;
+          ${i}        `
+        i++;
         list.appendChild(li);
           });
   
-      
-      $('#crypto-ticker').carouselTicker({
-        speed: 0.05,
-        direction: 'down',
-        duplicated: true,
-        pauseOnHover: true
-      });
+    //   $(document).ready(function() {
+    //         $('.carouselTicker').carouselTicker();
+    //     });
+
+    //   $('.carouselTicker').carouselTicker({
+    //     speed: 0.05,
+    //     direction: 'down',
+    //     duplicated: true,
+    //     pauseOnHover: true
+    //   });
     }
   
 async function newsData() {
@@ -48,7 +55,7 @@ async function displayNews() {
     const data = await response.json();
     
     
-    data.articles.slice(0, 5).forEach((article) => {
+    data.articles.slice(0, 20).forEach((article) => {
             const articleElement = document.createElement('div');
             articleElement.classList.add('article');
             articleElement.innerHTML = `
